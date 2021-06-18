2ebd0d938ee3e2aee4e268042c3801e523820c1c
boroviksergey <85708258+boroviksergey@users.noreply.github.com>
2021-06-18 13:53:49 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Configure the Microsoft Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.63.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+# Create a resource group vor app
+resource "azurerm_resource_group" "rg1" {
+    name     = "Linux"
+    location = "eastus"
+}
+
+
