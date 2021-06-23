8eb0abba891e02618e161523cf2272b1a1d9ceed
boroviksergey <85708258+boroviksergey@users.noreply.github.com>
2021-06-23 11:33:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,4 +19,14 @@ resource "azurerm_resource_group" "rg1" {
     location = "eastus"
 }
 
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "linuxstorageaccount" {
+    name                        = "linsacc"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+    account_tier                = "Standard"
+    account_replication_type    = "RAGRS"
+}
 
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
