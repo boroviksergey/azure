b5f3a0f8c089244f02a3d375fe0377edb09cfe49
boroviksergey <85708258+boroviksergey@users.noreply.github.com>
2021-06-29 15:45:36 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ backend "azurerm" {
     resource_group_name  = "Linux"
     storage_account_name = "linsacc"
     container_name       = "tfcontainer"
-    key                  = "terraform.tfstate"
+    key                  = "+erMc2SuIMM40kezsAUD/vIvRH18srQ8Hf+BnofuDellc8ZalGVwEp5851JWktJ3o/5xvOYJ30NfMOTbjGGsEA=="
   }
 }
